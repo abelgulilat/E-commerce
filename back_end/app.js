a4ba0f06325e04dbb104b5ec2e47507ca5c75eb1
abelgulilat <abelgulilat111@gmail.com>
2024-10-14 13:37:13 +0300

    look

--- back_end/app.js
+++ back_end/app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(cors())
+app.use(cors({origin: '*',}))
 
 app.use("/api/v1/user",userRouter)
 app.use("/api/v1/category",category)
