950eb053120eb1beb85ea9e2249be6492b673c48
abelgulilat <abelgulilat111@gmail.com>
2024-10-14 13:45:15 +0300

    look2

--- front_end/vite.config.js
+++ front_end/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "https://e-commerce-6-in4t.onrender.com",
+      "/api/v1/": "https://e-commerce-6-in4t.onrender.com",
       "/uploads/": "https://e-commerce-6-in4t.onrender.com",
     },
   },
