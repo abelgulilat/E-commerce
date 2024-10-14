de32fe17e489e89ea2a3dc5ccf81696c01196191
abelgulilat <abelgulilat111@gmail.com>
2024-10-14 12:24:55 +0300

    next

--- back_end/app.js
+++ back_end/app.js
@@ -8,12 +8,16 @@ import productRoutes from "./route/productRoutes.js";
 import uploadRoutes from "./route/uploadRoutes.js";
 import orderRoutes from "./route/orderRoutes.js";
 
+import cors from "cors"
+
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use(cors())
 
 app.use("/api/v1/user",userRouter)
 app.use("/api/v1/category",category)
