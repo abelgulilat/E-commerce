6baaa35fe605cb2d28ff1b788e9290d1df12c915
abelgulilat <abelgulilat111@gmail.com>
2024-10-17 15:01:41 +0300

    test 2

--- front_end/src/redux/constants.js
+++ front_end/src/redux/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "";
+export const BASE_URL = "https://e-commerce-6-in4t.onrender.com";
 export const USERS_URL = "/api/v1/user";
 export const CATEGORY_URL = "/api/v1/category";
 export const PRODUCT_URL = "/api/v1/products";
--- front_end/vite.config.js
+++ front_end/vite.config.js
@@ -4,19 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
+  // server: {
+  //   proxy: {
 
-      "/api": {
-          target:"https://e-commerce-6-in4t.onrender.com",
-          changeOrigin:true,
-      },
-      "/uploads": {
-          target:"https://e-commerce-6-in4t.onrender.com",
-          changeOrigin:true,
-      }
+  //     "/api": {
+  //         target:"https://e-commerce-6-in4t.onrender.com",
+  //         changeOrigin:true,
+  //     },
+  //     "/uploads": {
+  //         target:"https://e-commerce-6-in4t.onrender.com",
+  //         changeOrigin:true,
+  //     }
     
-    },
+  //   },
 
-  }
+  // }
 })
