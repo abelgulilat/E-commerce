c1500d620a80ae3b3a530a2aa9000d8e720f960b
abelgulilat <abelgulilat111@gmail.com>
2024-10-18 07:21:37 +0300

    g

--- back_end/utils/createToken.js
+++ back_end/utils/createToken.js
@@ -11,7 +11,7 @@ const generateToken = (res, userId) => {
     res.cookie("jwt", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV !== "development",
-      sameSite: "none",
+      sameSite: "None",
       maxAge: 30 * 24 * 60 * 60 * 1000,
     });
 
