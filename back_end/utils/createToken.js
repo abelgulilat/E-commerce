f0abda2340a57ae0e9ecdb6f8b475eaab4d9b9f5
abelgulilat <abelgulilat111@gmail.com>
2024-10-28 07:10:11 +0300

    ������

--- back_end/utils/createToken.js
+++ back_end/utils/createToken.js
@@ -10,7 +10,7 @@ const generateToken = (res, userId) => {
   // Set JWT as an HTTP-Only Cookie
     res.cookie("jwt", token, {
       httpOnly: false,
-      secure: process.env.NODE_ENV !== "development",
+      secure: true,
       sameSite: "None",
       maxAge: 30 * 24 * 60 * 60 * 1000,
     });
